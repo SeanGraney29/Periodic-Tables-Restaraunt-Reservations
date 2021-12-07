10a6e5d84b87766cd33db459231b48ec438898e1
SeanGraney29 <91019071+SeanGraney29@users.noreply.github.com>
2021-12-06 18:21:19 -0800

    Update api.js

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,7 @@
  import formatReservationTime from "./format-reservation-date";
  
  const API_BASE_URL =
-   process.env.REACT_APP_API_BASE_URL || "https://graney-final-capstone-backend.herokuapp.com/";
+   process.env.REACT_APP_API_BASE_URL || "https://graney-final-capstone-backend.herokuapp.com";
  
  /**
   * Defines the default headers for these functions to work with `json-server`
